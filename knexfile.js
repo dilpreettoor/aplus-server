521b495c5e4fb986c66d9da4c06c9572f7e388e5
dilpreettoor <dilpreet.s.t@gmail.com>
2023-09-08 01:58:41 -0400

    adding heroku again

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.POSTGRES_URL, 
+    connection: process.env.DATABASE_URL, 
     migrations: {
       directory: './migrations',
     },
--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
   connection: {
-    host : process.env.DB_HOST,
+    host : process.env.DATABASE_URL,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME
